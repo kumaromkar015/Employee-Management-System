599ba72fdcdd5e1e2f4533f62969a8942dc7504e
kumaromkar015 <kumaromkar015@gmail.com>
2026-05-16 16:29:56 +0530

    some update related to lambok issue

--- backend/src/main/java/com/example/EmployeeManagementSystem/backend/dto/EmployeeDTO.java
+++ backend/src/main/java/com/example/EmployeeManagementSystem/backend/dto/EmployeeDTO.java
@@ -24,6 +24,35 @@ public class EmployeeDTO {
 	private double salary;
 
 
-	
-	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
 }
--- backend/src/main/java/com/example/EmployeeManagementSystem/backend/entity/EmployeeEntity.java
+++ backend/src/main/java/com/example/EmployeeManagementSystem/backend/entity/EmployeeEntity.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Data  // getter and setter , hascode() , toStirng() , equals() 
+
 @Entity
 @Table(name = "EmployeeRecord")
 public class EmployeeEntity {
@@ -27,4 +27,43 @@ public class EmployeeEntity {
 	private String department;
 	private double salary;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
 }
